1a92e0e480ed23f45c0feecb7f7e5aa8c6c7dfde
Popa Alex <popa.alex1@gmail.com>
2016-01-12 05:05:19 -0800

    web: java -Dserver.port=$PORT -jar target/<firstherokuap>-0.0.1-SNAPSHOT.jar

---
+++ firstherokuap/src/main/java/ro/sci/SimpleLogInApplication.java
@@ -0,0 +1,13 @@
+package ro.sci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLogInApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SimpleLogInApplication.class, args);
+    }
+
+}
